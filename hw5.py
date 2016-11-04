44ada22dcd1a47a53a1f3a5a4860bd11b1e0d6e3
Nick William Philip Franzese <nick.franzese@gmail.com>
2016-11-04 04:04:45 -0700

    added assignment comments for each clique

--- hw5.py
+++ hw5.py
@@ -68,8 +68,17 @@ def main():
 
     CL_graph, clnodes, cledges = utils.parse_input('MI_0030_cross-linking_study.txt','\t',isWeighted=True)
     CL_cliques = greedy_clique_partition(CL_graph.better_adj_ls())
-    print(CL_cliques)
     print(CL_cliques[0])
+    #largest clique contains:
+    #Transforming growth factor beta receptor type 3
+    #    most significant p value GO term (.396): epicardium-derived cardiac fibroblast cell development
+    #          i.e. this is non-significantly implicated in maturation of cells that comprise some type of connective tissue in the heart
+    #TGF-beta receptor type 2
+    #    most significant p value GO term (.396): positive regulation of tolerance induction to self antigen
+    #        the description is sparse but I imagine this term is for things that play a part in keeping immune cells from targetting the body
+    #Serine/threonine-protein kinase receptor R3
+    #    most significant p value GO term (1.00): blood vessel endothelial cell proliferation involved in sprouting angiogenesis
+    #        this is the first term on the list but all of them are p = 1.00 so that's a little mysterious. I guess I can talk a little about the function of the term itself. Angiogenesis is creation of blood vessels. Blood vessels have endothelial cells. When you create blood vessels you gotta proliferate some endothelial cells.
 
 
 
@@ -82,7 +91,17 @@ def main():
     NMR_graph, nmrnodes, nmredges = utils.parse_input('MI_0077_nuclear_magnetic_resonance.txt','\t',isWeighted=True)
     NMR_cliques = greedy_clique_partition(NMR_graph.better_adj_ls())
     print(NMR_cliques[0])
+    #largest clique contains:
+    #Cytoplasmic protein NCK2
+    #    most significant p value GO term (1.00): negative regulation of transcription from RNA polymerase II promoter in response to endoplasmic reticulum stress
+    #        Again, this is the first one on the list but they all have p = 1.00 so I'm not really sure what to make of that. I guess that's a sign that the protein is poorly characterized which makes sense given that it's called 'Cytoplasmic protein NCK2'. Negative regulation of transcription from RNA polymerase II in response to ER stress would make sense, since ER stress is bound to cause changes to the transcriptome.
+    #Polyubiquitin-C
+    #    most significant p value GO term (.792): ubiquitin homeostasis
+    #        non-significantly involved in maintinence of ubiquitin I guess. Ubiquitin is a protein that tags other proteins for degredation.
 
+
+
+    
 
     #X-ray crystallography: The biochemical gold standard for protein characterization.
     #First you figure out the conditions under which your protein crystallizes (this can take awhile) and then you shoot x-rays at the crystal. This produces a diffraction pattern. Feed the pattern into
@@ -91,6 +110,18 @@ def main():
     XRC_graph, xrcnodes, xrcedges = utils.parse_input('MI_0114_x-ray_crystallography.txt','\t',isWeighted=True)
     XRC_cliques = greedy_clique_partition(XRC_graph.better_adj_ls())
     print(XRC_cliques[0])
+    #largest clique contains:
+    #Cellular tumor antigen p53
+    #   most significant p value GO term (.729): oligodendrocyte apoptotic process
+    #       an oligodendrocyte is a type of cell that forms the myelin sheath for neurons. apoptosis is programmed cell death.
+    #Ubiquitin carboxyl-terminal hydrolase 7
+    #   most significant p value GO term (1.00): maintinence of DNA methylation
+    #       DNA methylation appears to somehow regulation transcription but I don't think there's a consensus on exactly how. I think in a genomics class I've been told that methylation usually causes lower rates of transcription.
+    #E3 ubiquitin-protein ligase Mdm2
+    #   most significant p value GO term (.396): cellular response to vitamin B1
+    #       cells need to respond to chemotaxis in order to effectively regulate their metabolism and other aspects of homeostasis. This is presumably one piece of this process.
+
+    
     
 if __name__ == '__main__':
     main()
